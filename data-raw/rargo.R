e7070d61693ef3e0b89686e9f084eba4ba8f2cb4
Michael Sumner <mdsumner@gmail.com>
2018-05-16 22:11:00 +1000

    half finished indexing mind melt

--- data-raw/rargo.R
+++ data-raw/rargo.R
@@ -1,24 +1,32 @@
-# u1 <- "http://www.usgodae.org/ftp/outgoing/argo/latest_data/D20180209_prof.nc"
-# u2 <- "http://www.usgodae.org/ftp/outgoing/argo/latest_data/D20180209_prof_0.nc"
+#u1 <- "http://www.usgodae.org/ftp/outgoing/argo/latest_data/D20180209_prof.nc"
+u1 <- "http://www.usgodae.org/ftp/outgoing/argo/latest_data/D20180209_prof_0.nc"
 # curl::curl_download(u1, file.path("data-raw", basename(u1)))
 #
 # curl::curl_download(u2, file.path("data-raw", basename(u2)))
-#f1 <- file.path("data-raw", "D20180209_prof.nc")
-f1 <- file.path("data-raw", "20180507_prof.nc")
+#u1 <- "http://data.nodc.noaa.gov/argo/gadr/data/csiro/5903948/nodc_5903948_prof.nc"
+#curl::curl_download(u1, file.path("data-raw", basename(u1)))
+f1 <- file.path("data-raw", basename(u1))
 library(RNetCDF)
 library(raster)
 
 nc <- RNetCDF::open.nc(f1)
-
 ## we can't get 1D vars with raster
-lon <- RNetCDF::var.get.nc(nc, "LONGITUDE")
-lat <- RNetCDF::var.get.nc(nc, "LATITUDE")
-juld <- RNetCDF::var.get.nc(nc, "JULD")
-temp <- t(raster(f1, varname = "TEMP"))
-pres <- t(raster(f1, varname = "PRES"))
+lon <- RNetCDF::var.get.nc(nc, toupper("LONGITUDE"))
+lat <- RNetCDF::var.get.nc(nc, toupper("LATITUDE"))
+juld <- RNetCDF::var.get.nc(nc, toupper("JULD"))
+platform <- RNetCDF::var.get.nc(nc, toupper("PLATFORM_NUMBER"))
+
+temp <- t(raster(f1, varname = toupper("TEMP")))
+pres <- t(raster(f1, varname = toupper("PRES")))
 ## and set extent to 0, ncol, 0, nrow
 temp <- setExtent(temp, extent(0, ncol(temp), 0, nrow(temp)))
 pres <- setExtent(pres, extent(temp))
+
+## get generalize groupping later
+groupi <- platform == unique(platform)[1]
+temp <- crop(temp, extent(temp, 1, nrow(temp), 1, 183))
+pres <- crop(pres, extent(pres, 1, nrow(pres), 1, 183))
+
 
 ## pres is right, negated for the Z on the qm
 qm <- quadmesh::quadmesh(pres, z = 0-pres) ## need 0- because bug in raster
@@ -28,12 +36,18 @@ qm$material <- list(col = viridis::viridis(100)[scales::rescale(extract(temp, t(
 
 
 ## no return from this point, if needs to be redone recreate qm
-qm$vb[1, ] <- extract(setValues(temp, rep(lon, nrow(temp))), t(qm$vb[1:2, ]),
+X <- extract(setValues(temp, rep(lon[1:183], nrow(temp))), t(qm$vb[1:2, ]),
                       method = "bilinear")
-qm$vb[2, ] <- extract(setValues(temp, rep(lat, nrow(temp))), t(qm$vb[1:2, ]),
+Y <- extract(setValues(temp, rep(lat[1:183], nrow(temp))), t(qm$vb[1:2, ]),
                       method = "bilinear")
-qm$vb[1:2, ] <- t(rgdal::project(t(qm$vb[1:2, ]), "+proj=laea +lon_0=-180"))
+qm$vb[1:2, ] <- t(rgdal::project(cbind(X, Y), "+proj=laea +lon_0=-174 +lat_0=-39"))
 
+
+## plotting in 3d is one thing
+## but for 2D we need quads as triangles
+#qm$ib[,1]
+
+plot(t(qm$vb[, qm$ib[,1000]]))
 ## first example needed filtering out a longitude range
 #bad <- qm$vb[1, ] < 100
 #qm$vb[, bad] <- NA
